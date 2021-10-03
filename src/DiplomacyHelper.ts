a7e8ba62b91bc52f54ac892e5ae0b0edcd75184e
Joshua Oster-Morris <josh@craftycoder.com>
2021-10-03 09:54:11 -0400

    update with zevs latest

--- src/DiplomacyHelper.ts
+++ src/DiplomacyHelper.ts
@@ -145,7 +145,12 @@ export class DiplomacyHelper extends LitElement {
               </p>
             </div>
             <figure>
-              <img src="Vdip.png" alt="VDiplomacy" width="600" height="500" />
+              <img
+                src="assets/Vdip.png"
+                alt="VDiplomacy"
+                width="600"
+                height="500"
+              />
               <figcaption class="caption">
                 <h5>
                   There are too many variants for you to get anything out of a
@@ -155,81 +160,106 @@ export class DiplomacyHelper extends LitElement {
               </figcaption>
             </figure>
           </div>
-          <div id="redscape" class="content-padding bottom-border">
-            <h3><a href="http://www.redscape.com/">Redscape</a></h3>
+          <!-- <div id="redscape" class="content-padding bottom-border">
+              <h3><a href = "http://www.redscape.com/">Redscape</a></h3>
+              <div class="play-text">
+                  <p>A forum based play by email type ordeal. I don't know much more about since I didn't register(I've already registered on WebDiplomacy in the<br />name of this website, thank you very much), but that shoud be enough to get you started.</p>
+              </div>
+              <figure>
+                  <img src="assets/Red.png" alt="Redscape" width = "600" height="500">
+                  <figcaption class="caption"><h5>Redscape Homepage. redscape.com</h5></figcaption>
+              </figure>
+          </div> -->
+          <div id="Dipgame" class="content-padding bottom-border">
+            <h3><a href="http://www.dipgame.net/">Dipgame</a></h3>
             <div class="play-text">
               <p>
-                A forum based play by email type ordeal. I don't know much more
-                about since I didn't register(I've already registered on
-                WebDiplomacy in the<br />name of this website, thank you very
-                much), but that shoud be enough to get you started.
+                Decent Interface all around, like backstabbr, except for the
+                added benefit of having really good bots that can do the
+                diplomatic part.
               </p>
             </div>
             <figure>
               <img
-                src="assets/Red.png"
-                alt="Redscape"
+                src="assets/DipGame.png"
+                alt="DipGame"
                 width="600"
                 height="500"
               />
               <figcaption class="caption">
-                <h5>Redscape Homepage. redscape.com</h5>
+                <h5>The Hompage's sample game. dipgame.net</h5>
               </figcaption>
             </figure>
           </div>
+          <div class="link-list bottom-border content-padding">
+            <h3>Play-By-Email Games</h3>
+            <p>For people who want to take their game like this:</p>
+            <ul>
+              <li><a href="http://www.dipbounced.com/">Bounced</a></li>
+              <li>
+                <a href="http://www.diplomaticcorp.com/">Diplomaticcorp</a>
+              </li>
+              <li>
+                <a href="http://www.redscape.com/">Redscape</a
+                ><span>, which is forum based.</span>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <!---
-      <div>
-          <h1>Currently Unworking Sites</h1>
-          <hr />
-          <h3><a href="http://www.amarriner.com/dip/">amarriner</a></h3>
-          <p>The guy that runs the <a href="http://www.amarriner.com/">wider site</a> is too busy to work on it often, so most of the stuff is <br />old tech that doesn't work, including his diplomacy engine. Put he is going to get off work SOMEday, so the option will probably be open with time.</p>
-          <p>Util that time, here's a picture of the sites homepage:</p>
-          <img src="assets/Pictures/amarrier.png" alt = "amarrier" width = "600" height="500">
-          <hr />
-          <h3>Nexus Diplomacy</h3>
-          <p>I don't really know if Nexus doesn't work, it simply takes such an unacetablly long time to load that I quit trying. I don't know the first thing about it, not even a link.</p>
-          <hr />
-          <h3><a href="http://stabbeurfou.org/">stabbeurfou</a></h3>
-          <p>Just another free website, but they ran short on donations and google kicked them out. Maybe a family member will come in and pay their debts<br />and refill their funds, so who knows?</p>
-          <figure>
-              <img src="assets/Pictures/a.png" alt = "They can't afford to stay on :(" width="600" height="500">
-              <figcaption>They can't afford to stay on :(</figcaption>
-          </figure>
-      </div>
-      --->
-      <div id="Sources">
-        <hr class="line" />
-        <h2>Sources</h2>
-        <p>
-          I did not know of all of these websites by myself. My main Importance
-          Is gathering everyone's <br />hard work into a concentrated space. Not
-          including the sites already mentioned, here are those sites:
-        </p>
-        <ul>
-          <li>
-            <a
-              href="https://boardgamegeek.com/thread/980251/best-place-play-diplomacy-online"
-              >boardgamegeek</a
-            >
-          </li>
-          <li>
-            <a href="https://diplomacygames.com/where-play-diplomacy/"
-              >diplomacygames</a
-            >
-          </li>
-          <li>
-            <a href="https://www.ultraboardgames.com/diplomacy/links.php"
-              >ultraboardgames</a
-            >
-          </li>
-        </ul>
-        <hr class="line" />
-      </div>
-      <div id="updated">
-        <hr class="line" />
-        <p>Last updated on September 30.</p>
+        <!--Maybe build a list of play by emails here?-->
+        <!---
+        <div>
+            <h1>Currently Unworking Sites</h1>
+            <hr />
+            <h3><a href="http://www.amarriner.com/dip/">amarriner</a></h3>
+            <p>The guy that runs the <a href="http://www.amarriner.com/">wider site</a> is too busy to work on it often, so most of the stuff is <br />old tech that doesn't work, including his diplomacy engine. Put he is going to get off work SOMEday, so the option will probably be open with time.</p>
+            <p>Util that time, here's a picture of the sites homepage:</p>
+            <img src="assets/Pictures/amarrier.png" alt = "amarrier" width = "600" height="500">
+            <hr />
+            <h3>Nexus Diplomacy</h3>
+            <p>I don't really know if Nexus doesn't work, it simply takes such an unacetablly long time to load that I quit trying. I don't know the first thing about it, not even a link.</p>
+            <hr />
+            <h3><a href="http://stabbeurfou.org/">stabbeurfou</a></h3>
+            <p>Just another free website, but they ran short on donations and google kicked them out. Maybe a family member will come in and pay their debts<br />and refill their funds, so who knows?</p>
+            <figure>
+                <img src="assets/Pictures/a.png" alt = "They can't afford to stay on :(" width="600" height="500">
+                <figcaption>They can't afford to stay on :(</figcaption>
+            </figure>
+        </div>
+        --->
+        <div class="link-list bottom-border content-padding">
+          <hr class="line" />
+          <h2>Sources</h2>
+          <p>
+            I did not know of all of these websites by myself. My main
+            Importance Is gathering everyone's <br />hard work into a
+            concentrated space. Not including the sites already mentioned, here
+            are those sites:
+          </p>
+          <ul>
+            <li>
+              <a
+                href="https://boardgamegeek.com/thread/980251/best-place-play-diplomacy-online"
+                >boardgamegeek</a
+              >
+            </li>
+            <li>
+              <a href="https://diplomacygames.com/where-play-diplomacy/"
+                >diplomacygames</a
+              >
+            </li>
+            <li>
+              <a href="https://www.ultraboardgames.com/diplomacy/links.php"
+                >ultraboardgames</a
+              >
+            </li>
+          </ul>
+          <hr class="line" />
+        </div>
+        <div id="updated">
+          <hr class="line" />
+          <p>Last updated on October 3.</p>
+        </div>
       </div>
     `;
   }
