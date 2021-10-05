f30f75a0ba3de5ce21a3b1fbe5a2ac186e15f327
Zev Oster <zevelijah@gmail.com>
2021-10-05 14:07:47 -0400

    delete useless code

--- src/DiplomacyHelper.ts
+++ src/DiplomacyHelper.ts
@@ -181,11 +181,6 @@ export class DiplomacyHelper extends LitElement {
         <div id="updated" class="bottom-border">
           <p>Last updated on October 3.</p>
         </div>
-        <!--Please don't destroy this just because it's javascript. This code was written by free-counters.org.-->
-        <div>
-          <script type="text/javascript" src="https://www.free-counters.org/count/9i9g"></script><br>
-          <a href='https://www.free-counters.org/'>https://www.free-Counters.org</a> <script type='text/javascript' src='https://www.whomania.com/ctr?id=fe82132c3d005f2fba31d526d7c8bb4f55ed00c7'></script>
-        </div>
       </div>
     `;
   }
